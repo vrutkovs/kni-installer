e400f6126f197814e4d4cffcd2f36e7c5ac2b701
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-03-13 09:58:33 +0100

    ClusterID -> InfraID

--- pkg/asset/machines/master.go
+++ pkg/asset/machines/master.go
@@ -133,7 +133,7 @@ func (m *Master) Generate(dependencies asset.Parents) error {
 		mpool.Set(ic.Platform.BareMetal.DefaultMachinePlatform)
 		mpool.Set(pool.Platform.BareMetal)
 		pool.Platform.BareMetal = &mpool
-		machines, err = baremetal.Machines(clusterID.ClusterID, ic, pool, "master", "master-user-data")
+		machines, err = baremetal.Machines(clusterID.InfraID, ic, pool, "master", "master-user-data")
 		if err != nil {
 			return errors.Wrap(err, "failed to create master machine objects")
 		}
